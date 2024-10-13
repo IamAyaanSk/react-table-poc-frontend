683596573905f25a04e8866d928b185095109a71
Ayaan Shaikh <63299939+IamAyaanSk@users.noreply.github.com>
2024-10-13 14:55:47 +0530

    .

--- src/app/ledgers/page.tsx
+++ src/app/ledgers/page.tsx
@@ -72,7 +72,7 @@ export default async function WalletLedgerTable({
                 ],
               },
             },
-            showDateRange: false,
+
             columnVisibilityConfig: {
               amount: {
                 ADMIN: ["ADMIN"],
